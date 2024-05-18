d4946c1770d03ce92cc1f8de82058b19b34f9121
bushra-n <160867479+bushra-n@users.noreply.github.com>
2024-05-18 17:13:43 +0500

    Add files via upload

---
+++ task23.js
@@ -0,0 +1,27 @@
+"use strict";
+// task 23 condition test: 5test true and 5 tests false
+let car = "Subaru";
+console.log("Is car == 'Subaru'? predict true");
+console.log(car == 'Subaru');
+console.log("Is car !='Honda city'?predict true");
+console.log(car != 'Honda city');
+console.log("Is car == 'Subaru'? predict false");
+console.log(car == 'Subaru');
+console.log("Is car == 'subaru'? predict true");
+console.log(car == 'subaru');
+console.log("Is car == 'SUBARU'? predict false");
+console.log(car == 'SUBARU');
+console.log("Is car.length == (6)?predict true");
+console.log(car.length == 6);
+console.log("Is car.length != (6)?predict false");
+console.log(car.length != 6);
+console.log("Is car.length == (10)?predict false");
+console.log(car.length == 10);
+console.log("Is car.length != (10)?predict true");
+console.log(car.length != 10);
+console.log("is 10 > 45? predict false");
+console.log(10 > 45);
+console.log("is 3 <= 2? predict false");
+console.log(3 <= 2);
+console.log("is car == 'Subaru' && car.length == 6?predict true");
+console.log(car == 'Subaru' && car.length == 6);
